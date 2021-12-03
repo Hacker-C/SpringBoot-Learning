e7e9426507f135d8851fd2110c1a3c89b26eb9c3
Hacker-C <1507559148@qq.com>
2021-12-03 19:34:41 +0800

    springboot login

---
+++ springboot-login/README.md
@@ -0,0 +1,4 @@
+# springboot 登录接口
+
+练习使用springboot写一个简单的登录接口
+
---
+++ springboot-login/src/main/java/top/mphy/springbootlogin/SpringbootLoginApplication.java
@@ -0,0 +1,13 @@
+package top.mphy.springbootlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLoginApplication.class, args);
+    }
+
+}
